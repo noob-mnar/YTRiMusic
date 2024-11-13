11089c0e6d0fa57a4ac617445934f20b6725bb24
twistios <54811436+twistios@users.noreply.github.com>
2024-11-14 00:03:48 +0100

    cleanup of BrotliEncoder.kt

--- extensions/ktor-client-brotli/src/main/kotlin/io/ktor/client/plugins/compression/BrotliEncoder.kt
+++ extensions/ktor-client-brotli/src/main/kotlin/io/ktor/client/plugins/compression/BrotliEncoder.kt
@@ -5,7 +5,6 @@ import io.ktor.utils.io.ByteReadChannel
 import io.ktor.utils.io.ByteWriteChannel
 import io.ktor.utils.io.jvm.javaio.toByteReadChannel
 import io.ktor.utils.io.jvm.javaio.toInputStream
-import kotlinx.coroutines.CoroutineScope
 import org.brotli.dec.BrotliInputStream
 import kotlin.coroutines.CoroutineContext
 
@@ -22,19 +21,13 @@ internal object BrotliEncoder : ContentEncoder {
         source: ByteReadChannel,
         coroutineContext: CoroutineContext
     ): ByteReadChannel {
-        TODO("Not yet implemented")
+         error("BrotliOutputStream not available (https://github.com/google/brotli/issues/715)")
     }
 
     override fun encode(
         source: ByteWriteChannel,
         coroutineContext: CoroutineContext
     ): ByteWriteChannel {
-        TODO("Not yet implemented")
+        error("BrotliOutputStream not available (https://github.com/google/brotli/issues/715)")
     }
-
-//    override fun CoroutineScope.encode(source: ByteReadChannel) =
-//        error("BrotliOutputStream not available (https://github.com/google/brotli/issues/715)")
-//
-//    override fun CoroutineScope.decode(source: ByteReadChannel): ByteReadChannel =
-//        BrotliInputStream(source.toInputStream()).toByteReadChannel()
 }
