996448c06965b50128cc8452fd34ebdc6c9f3e26
knighthat <knightchanelgaming@gmail.com>
2024-11-26 00:00:45 -0600

    move size to default constructor

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/CarousalSize.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/CarousalSize.kt
@@ -1,19 +1,9 @@
 package it.fast4x.rimusic.enums
 
-enum class CarouselSize {
-    Small,
-    Medium,
-    Big,
-    Biggest,
-    Expanded;
-
-    val size: Int
-        get() = when (this) {
-            Small -> 90
-            Medium -> 55
-            Big -> 30
-            Biggest -> 20
-            Expanded -> 0
-        }
-
+enum class CarouselSize( val size: Int ) {
+    Small( 90 ),
+    Medium( 55 ),
+    Big( 30 ),
+    Biggest (20 ),
+    Expanded( 0 );
 }
